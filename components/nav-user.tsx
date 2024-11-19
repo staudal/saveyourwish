eb431b74a070f344fc2ebba795258657a6313675
Jakob Staudal <jakobstaudal@outlook.com>
2024-11-19 09:52:56 +0100

    Remove unused imports

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -1,15 +1,8 @@
 "use client";
 
-import {
-  BadgeCheck,
-  Bell,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
+import { BadgeCheck, Bell, ChevronsUpDown, LogOut } from "lucide-react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
