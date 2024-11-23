b37403639e584e1535a0ac8211004b5755e0ae1a
Jakob Staudal <jakobstaudal@outlook.com>
2024-11-23 10:43:54 +0100

    Update main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p>
-        Currently not implemented. Please visit{" "}
-        <Link href="/dashboard">/dashboard</Link> to get started.
-      </p>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          Currently not implemented. Please visit{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            <Link href="/dashboard">/dashboard</Link>
+          </code>{" "}
+          to get started.
+        </p>
+      </main>
     </div>
   );
 }
