a342c5a8dbffcd3b01f6c727e4a2c3c630603307
Jakob Staudal <jakobstaudal@outlook.com>
2024-12-18 11:11:56 +0100

    Update from min-h-svh to min-h-screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ export default async function LoginPage() {
   }
 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link
           href="/"
