ba11d7cb2d3409b2d6743cd17cfb9718a746ebcb
Jakob Staudal <jakobstaudal@outlook.com>
2024-12-18 10:06:11 +0100

    Fix viewport settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,13 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
+  minimumScale: 1,
   userScalable: false,
+  viewportFit: "cover",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
